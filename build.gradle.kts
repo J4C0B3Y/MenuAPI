03b5118f59a0cacafa94b3b522e9206b8120fc9d
J4C0B3Y <J4C0B3Y@gmail.com>
2024-12-29 16:20:11 +1100

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ object Project {
     const val NAME = "MenuAPI"
     const val GROUP = "net.j4c0b3y"
     const val AUTHOR = "J4C0B3Y"
-    const val VERSION = "1.3.0"
+    const val VERSION = "1.4.0"
 }
 
 repositories {
