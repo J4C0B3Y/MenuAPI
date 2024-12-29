b90645d1d40c5ce08f0331ba34e77924f4833e94
J4C0B3Y <J4C0B3Y@gmail.com>
2024-12-29 21:01:05 +1100

    remove ButtonClick#allow, separate getters

--- src/main/java/net/j4c0b3y/api/menu/button/ButtonClick.java
+++ src/main/java/net/j4c0b3y/api/menu/button/ButtonClick.java
@@ -3,9 +3,9 @@ package net.j4c0b3y.api.menu.button;
 import lombok.AccessLevel;
 import lombok.Getter;
 import net.j4c0b3y.api.menu.Menu;
-import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Stores information about a player clicking a menu.
@@ -19,7 +19,8 @@ public class ButtonClick {
     /**
      * The underlying click event.
      */
-    @Getter(AccessLevel.NONE) private final InventoryClickEvent event;
+    @Getter(AccessLevel.NONE)
+    private final InventoryClickEvent event;
 
     /**
      * The menu that was clicked.
@@ -33,22 +34,6 @@ public class ButtonClick {
     private final Button button;
 
     /**
-     * The player that clicked.
-     */
-    private final Player player;
-
-    /**
-     * The type of the click.
-     */
-    private final ClickType type;
-
-    /**
-     * The click slot, this is -1 if the
-     * click was outside the inventory.
-     */
-    private final int slot;
-
-    /**
      * Creates a new button click, extracting
      * values from the underlying click event.
      *
@@ -60,15 +45,26 @@ public class ButtonClick {
         this.event = event;
         this.menu = menu;
         this.button = button;
-        this.player = menu.getPlayer();
-        this.type = event.getClick();
-        this.slot = event.getSlot();
     }
 
     /**
-     * Allows pickup / movement of the button item.
+     * @return The type of click.
      */
-    public void allow() {
-        event.setCancelled(false);
+    public ClickType getType() {
+        return event.getClick();
+    }
+
+    /**
+     * @return The slot of the click.
+     */
+    public int getSlot() {
+        return event.getSlot();
+    }
+
+    /**
+     * @return The item in the cursor.
+     */
+    public ItemStack getCursor() {
+        return event.getCursor();
     }
 }
