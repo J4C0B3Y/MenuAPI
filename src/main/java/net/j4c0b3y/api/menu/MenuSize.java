1e0585dcf221b78314a6f960ed917735cc08fa30
J4C0B3Y <J4C0B3Y@gmail.com>
2025-02-04 00:52:04 +1100

    add MenuSize#of method

--- src/main/java/net/j4c0b3y/api/menu/MenuSize.java
+++ src/main/java/net/j4c0b3y/api/menu/MenuSize.java
@@ -37,4 +37,23 @@ public enum MenuSize {
      * How many rows the menu has.
      */
     private final int rows;
+
+    /**
+     * Returns the menu size using the input number,
+     * which can be either the amount of rows or slots.
+     *
+     * @param input The rows or slots.
+     * @return The associated menu size.
+     */
+    public static MenuSize of(int input) {
+        for (MenuSize size : MenuSize.values()) {
+            int rows = size.getRows();
+
+            if (rows == input || rows == input * Menu.COLUMNS) {
+                return size;
+            }
+        }
+
+        throw new IllegalArgumentException("There is no menu size with " + input + " rows or slots!");
+    }
 }
